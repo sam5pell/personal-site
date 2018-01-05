f1ef4e5053e1f464f13f96dbe6a00d6af3f5bc3f
Sam Pelletier <spelletier@college.harvard.edu>
2018-01-04 20:51:42 -0500

    navbar changes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home'});
+  res.render('index', { title: 'Home', active: 'index' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+  res.render('projects', { title: 'Projects', active: 'projects' });
 });
 
 module.exports = router;
